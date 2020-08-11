bafea3bef568f338a903177dfb9ccd6a0a0cc6c5
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-11 15:52:44 -0600

    trying a new way of going about what I'm aiming for

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -36,11 +36,32 @@ from graph import Graph
 #             s.push(new_node)
 
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def earliest_ancestor(ancestors, starting_node, visited=None, path=None):
+    
 
 # I have a list of ancestors which are individual sets
 # I either want to work up from the starting_node to the top or start at the top and try to find the starting_node
 # the ancestors are in sets with the first being the parent and the second being the child.
 
 # if I cycle through the node trying to find the starting node - if it is found I can store the pathway in a variable. At the end of the cycle I can find the longest pathway and return the first number in the first set.
+
+    # if visited == None and path == None:
+    #     visited = set()
+    #     longest_path
+    #     path = []
+    
+    # if len(ancestors) == 1:
+    #     return path
+    
+    # for p_c in ancestors:
+    #     print(p_c[0])
+
+
+    s = Stack()
+    visited = set()
+    longest_path = []
+    longest_length = 0
+    s.push(starting_node)
+
+    while s.size() > 0:
+        path = s.pop()  
