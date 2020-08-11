f23348a13f55f24dd285f0d73754caaeb61486c1
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-11 14:22:10 -0600

    looking to go about solving using length of path

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,5 +1,18 @@
 from stack import Stack
 
 def earliest_ancestor(ancestors, starting_node):
+    longest_path = 0
+    ancestors = None
     s = Stack()
     visited = set()
+
+    s.push([starting_node])
+    while s.size() > 0:
+        path = s.pop()
+        check_path = path[-1]
+
+        if check_path not in visited:
+            visited.add(check_path)
+        if len(check_path) > longest_path:
+            longest_path = len(check_path)
+            
