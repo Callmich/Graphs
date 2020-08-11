71d583259896e1e932236f7124d5002b1de2dc6d
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-11 14:43:43 -0600

    I think I have confised myself very well - I need to restart my uper process. I kinda went straight into coding

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,10 +1,21 @@
 from stack import Stack
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
     longest_path = 0
     ancestors = []
     s = Stack()
     visited = set()
+
+    def get_parent(vertx):
+    parents = []
+    for x in ancestors:
+        check = Graph.get_neighbor(x)
+        if check == vertx:
+            parents.append(x)
+    
+
 
     s.push([starting_node])
     while s.size() > 0:
@@ -19,7 +30,7 @@ def earliest_ancestor(ancestors, starting_node):
         elif len(check_path) == longest_path:
             ancestors = ancestors.append(check_path)
         
-        for parent in self.get_neighbors(check_path):
+        for parent in get_parent(check_path):
             new_node = list(path)
             new_node.append(parent)
             s.push(new_node)
