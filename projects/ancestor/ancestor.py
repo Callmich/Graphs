74d9022a8e5f228d66f1327c02fb208835f1ea7c
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-12 13:33:22 -0600

    adjustemnets made

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -36,43 +36,47 @@
 
 #     return longest_path[-1]
 
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertices(self, vert):
+        if vert not in self.vertices:
+            self.vertices[vert] = set()
+
+    def add_edge(self, vertI, vertII):
+        self.vertices[vertI].add(vertII)
+
+    def get_neighbors(self, vert):
+        return self.vertices[vert]
+
+
+def create_graph(ancestors):
+    graph = Graph()
+    for p, c in ancestors:
+        graph.add_vertices(p)
+        graph.add_vertices(c)
+        graph.add_edge(c, p)
+    return graph
+
 class Stack():
     def __init__(self):
         self.stack = []
-    
+
     def push(self, value):
         self.stack.append(value)
-    
+
     def pop(self):
         if self.size() > 0:
             return self.stack.pop()
         else:
             return None
-    
+
     def size(self):
         return len(self.stack)
 
-class Graph:
-    def __init__(self):
-        self.verticies = {}
-    
-    def add_verts(self, vert):
-        if vert not in self.verticies:
-            self.verticies[vert] = set()
-    
-    def add_edge(self, v1, v2):
-        self.verticies[v1].add(v2)
-
-    def get_neighbors(self, vert):
-        return self.verticies[vert]
-
-def create_graph(lst):
-    graph = Graph()
-    for pair in lst:
-        graph.add_verts(lst[0])
-        graph.add_verts(lst[1])
-        graph.add_edge(lst[1], lst[0])
-    return graph
 
 def earliest_ancestor(ancestors, starting_node):
     g = create_graph(ancestors)
@@ -80,21 +84,25 @@ def earliest_ancestor(ancestors, starting_node):
     s = Stack()
     visited = set()
     s.push([starting_node])
-    big_path = []
+    longest_path = []
 
     while s.size() > 0:
         path = s.pop()
-        node = path[-1]
+        current_node = path[-1]
 
-        if len(path) > len(big_path):
-            visited.add(node)
-            ps = g.get_neighbors(node)
-        
-        for p in ps:
-            path_copy = path+[p]
-            s.push(path_copy)
-    
-    if starting_node == big_path[-1]:
+        if len(path) > len(longest_path):
+            longest_path = path
+
+        if current_node not in visited:
+            visited.add(current_node)
+            family = g.get_neighbors(current_node)
+
+            for parent in family:
+                new_path = path+[parent]
+                s.push(new_path)
+    if starting_node == longest_path[-1]:
         return -1
     else:
-        return big_path[-1]
+        return longest_path[-1]
+
+
