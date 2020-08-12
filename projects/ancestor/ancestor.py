745c283639984a7ec556d15a1fba85c07c2ce4ca
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-12 09:16:15 -0600

    attempting to reset with using a graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,39 +1,67 @@
-from util import Stack
+# from util import Stack
 
 
-def earliest_ancestor(ancestors, starting_node):
+# def earliest_ancestor(ancestors, starting_node):
+
+#     verts = {}
+
+#     for each_set in ancestors:
+#         if each_set[1] in verts.keys():
+#             verts[each_set[1]].append(each_set[0])
+#         else:
+#             verts[each_set[1]] = [each_set[0]]
+
+#     s = Stack()
+#     s.push([starting_node])
+#     longest_path = []
     
+#     while s.size() > 0:
+#         path = s.pop()
+#         v = path[-1]
 
+#         if v not in verts.keys():
+#             if v == starting_node:
+#                 return -1
 
-    verts = {}
+#             if len(path) > len(longest_path):
+#                 longest_path = path
+#             elif len(path) == len(longest_path):
+#                 if v < longest_path[-1]:
+#                     longest_path = path
+#         else:
+#             for i in verts[v]:
+#                 copy_path = path.copy()
+#                 copy_path.append(i)
+#                 s.push(copy_path)
 
-    for each_set in ancestors:
-        if each_set[1] in verts.keys():
-            verts[each_set[1]].append(each_set[0])
-        else:
-            verts[each_set[1]] = [each_set[0]]
+#     return longest_path[-1]
 
-    s = Stack()
-    s.push([starting_node])
-    longest_path = []
+class Stack():
+    def __init__(self):
+        self.stack = []
     
-    while s.size() > 0:
-        path = s.pop()
-        v = path[-1]
-
-        if v not in verts.keys():
-            if v == starting_node:
-                return -1
-
-            if len(path) > len(longest_path):
-                longest_path = path
-            elif len(path) == len(longest_path):
-                if v < longest_path[-1]:
-                    longest_path = path
+    def push(self, value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
         else:
-            for i in verts[v]:
-                copy_path = path.copy()
-                copy_path.append(i)
-                s.push(copy_path)
+            return None
+    
+    def size(self):
+        return len(self.stack)
 
-    return longest_path[-1]
+class Graph:
+    def __init__(self):
+        self.verticies = {}
+    
+    def add_verts(self, vert):
+        if vert not in self.verticies:
+            self.verticies[vert] = set()
+    
+    def add_edge(self, v1, v2):
+        self.verticies[v1].add(v2)
+
+    def get_neighbors(self, vert):
+        return self.verticies[vert]
