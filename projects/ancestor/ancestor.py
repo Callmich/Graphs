2498cf4d186d1bf315cd725da0207d2d01d8a026
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-12 08:08:09 -0600

    tryed a differnt way of solving this - still failing. Not sure why

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -14,25 +14,22 @@ def earliest_ancestor(ancestors, starting_node):
         tree.add_edge(each_set[1], each_set[0])
 
     s = Stack()
+    s.push([starting_node])
+    longest_len = 1
+    earliest_anc = -1
     visited = set()
     longest_path = []
-    s.push([starting_node])
-
+    
     while s.size() > 0:
         path = s.pop()
         node = path[-1]
 
-        if len(path) > len(longest_path):
-            longest_path = path
+        if (len(path) >= longest_len and node < earliest_anc) or (len(path) > longest_len):
+            earliest_anc = node
+            longest_len = len(path)
         
-        if node not in visited:
-            visited.add(node)
-            ps = tree.get_neighbors(node)
-
-            for p in ps:
-                new_path = path+[p]
-                s.push(new_path)
-    if starting_node == longest_path[-1]:
-        return -1
-    else:
-        return longest_path[-1]
+        for ngbr in tree.vertices[node]:
+            new_path = list(path)
+            new_path.append(ngbr)
+            s.push(new_path)
+    return earliest_anc
