6c9cf219029bec9184d4ef61ce65b7572d93d137
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-13 21:52:59 -0600

    my built dictionary is showing all of the correct locations but the test is failing. I need to see what is causing that and really what makes a test pass

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -25,10 +25,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -75,7 +75,7 @@ def sprint_slayer(current_room, visited=None):
         # for loop for possible directions
         for direction in poss_dirs:
             # if an exit in a direction does not exist it removes it from the dictionary
-            if direction not in current.get_exits():
+            if direction not in current.get_exits() and direction in room_dict[current.id]:
                 room_dict[current.id].pop(direction)
         
         # for loop for available directions
